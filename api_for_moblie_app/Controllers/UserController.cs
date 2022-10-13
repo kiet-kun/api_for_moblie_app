ba9eee079ceb97ef54bc19545708920f914a416e
kiet <cnpmcs03@gmail.com>
2022-10-13 17:10:37 +0700

    Add project files.

---
+++ api_for_moblie_app/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using api_for_moblie_app.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_for_moblie_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static List<User> users = new List<User>
+        {
+           new User
+           {
+               Id=1,
+               Name = "Nguyen Hoang Kiet",
+               Age = 20,
+               Created = DateTime.Now
+           }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return Ok(users);
+        }
+    }
+
+   
+}
---
+++ api_for_moblie_app/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_for_moblie_app.Data
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0, 100)]
+        public int Age { get; set; }
+
+        public DateTime Created { get; set; }
+       
+    }
+}
